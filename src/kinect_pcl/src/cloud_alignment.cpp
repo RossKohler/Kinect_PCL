1e48e4d418173e2a0cfee78bb73c6858da571541
RossKohler <rossdkohler@gmail.com>
2015-07-31 12:43:05 +0200

    modify sac_ia method

--- src/kinect_pcl/src/cloud_alignment.cpp
+++ src/kinect_pcl/src/cloud_alignment.cpp
@@ -7,9 +7,37 @@
 #include <pcl/registration/icp.h>
 
 int sac_ia_alignment(pcl::PointCloud<pcl::PointXYZ>::Ptr prev_cloud,pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in){
-	std::cout << "Performing Sample Consensus Initial Alignment.. ";
+	std::cout << "Performing Sample Consensus Initial Alignment.. "<<std::endl;
 	FeatureCloud targetCloud;
+	FeatureCloud templateCloud;
 	targetCloud.setInputCloud(prev_cloud);
+	templateCloud.setInputCloud(cloud_in);
+
+	TemplateAlignment templateAlign;
+	templateAlign.addTemplateCloud(templateCloud);
+	templateAlign.setTargetCloud(targetCloud);
+
+	Result bestAlignment;
+
+	templateAlign.findBestAlignment(bestAlignment);
+
+	printf("Fitness Score: %f \n",bestAlignment.fitness_score);
+
+	Eigen::Matrix3f rotation = bestAlignment.final_transformation.block<3,3>(0,0);
+	Eigen::Matrix3f translation = bestAlignment.final_transformation.block<3,1>(0,3);
+
+	printf ("\n");
+	  printf ("    | %6.3f %6.3f %6.3f | \n", rotation (0,0), rotation (0,1), rotation (0,2));
+	  printf ("R = | %6.3f %6.3f %6.3f | \n", rotation (1,0), rotation (1,1), rotation (1,2));
+	  printf ("    | %6.3f %6.3f %6.3f | \n", rotation (2,0), rotation (2,1), rotation (2,2));
+	  printf ("\n");
+	  printf ("t = < %0.3f, %0.3f, %0.3f >\n", translation (0), translation (1), translation (2));
+
+	pcl::transformPointCloud(*templateCloud.getPointCloud(),*cloud_out,bestAlignment.final_transformation);
+
+	std::cout << "***Initial alignment complete***" << std::endl;
+
+
 
 
 	
